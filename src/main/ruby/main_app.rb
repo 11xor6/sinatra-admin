e0d0d72040d4537a31f36f65742cdffa9878a866
Nik Hodgkinson <nik@metamarketsgroup.com>
2012-01-02 04:00:59 -0800

    All that's left is some reasonable styling.

--- src/main/ruby/main_app.rb
+++ src/main/ruby/main_app.rb
@@ -1,14 +1,23 @@
 class MainApp
   require 'rubygems'
   require 'bundler'
-  require 'sinatra'
-  require 'haml'
-  require 'sass'
+  require 'sinatra'             # http://www.sinatrarb.com/documentation
+  require 'haml'                # http://haml-lang.com/docs/yardoc/file.HAML_REFERENCE.html
+  require 'sass'                # http://haml-lang.com/docs/yardoc/file.HAML_REFERENCE.html
+  require 'sinatra/ratpack'     # http://rubydoc.info/github/zeke/ratpack/master/Sinatra/Ratpack
+  require 'sinatra/content_for' # https://github.com/foca/sinatra-content-for
 
   get '/' do
+    content_for :css do
+      stylesheet_link_tag 'style'
+    end
     @title = "Hello"
-    @css = ""
+
     @include = ""
     haml :index
+  end
+
+  get '/stylesheets/style.css' do
+    scss :style
   end
 end
